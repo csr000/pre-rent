84c53c371803bc7d22b1c365b7064e63f579a783
csr000 <csrichard000@gmail.com>
2022-09-15 02:53:18 +0100

    💄 update map screen

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,13 +1,18 @@
 import React, { useCallback, useMemo, useRef } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
+
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+import Block from '../components/Block';
+import Text from '../components/Text';
+import colors from '../constants/colors';
 
 const Map = () => {
   // ref
   const bottomSheetRef = useRef(null);
 
   // variables
-  const snapPoints = useMemo(() => ['25%', '50%'], []);
+  const snapPoints = useMemo(() => ['2%', '35%'], []);
 
   // callbacks
   const handleSheetChanges = useCallback(index => {
@@ -16,18 +21,57 @@ const Map = () => {
 
   // renders
   return (
-    <View style={styles.container}>
+    <Block style={styles.container}>
       <BottomSheet
         ref={bottomSheetRef}
         index={1}
         snapPoints={snapPoints}
         onChange={handleSheetChanges}
       >
-        <View style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
-        </View>
+        <Block style={styles.contentContainer}>
+          <Block row center width="80%" space="between">
+            <Text>Img</Text>
+            <Block>
+              <Text bold>Steve Torn</Text>
+              <Text>Your manager</Text>
+            </Block>
+            <AntDesign name="message1" size={24} color={colors.black} />
+            <Ionicons name="call" size={24} color={colors.black} />
+          </Block>
+          <Block row center width="80%" space="between">
+            <Text bold size={18}>
+              Remaining time
+            </Text>
+            <Block row center>
+              <Text bold size={48}>
+                1
+              </Text>
+              <Text>hr </Text>
+              <Text bold size={48}>
+                05
+              </Text>
+              <Text>min</Text>
+            </Block>
+          </Block>
+          <Block row center width="80%" space="between">
+            <Text size={18}>11:00AM</Text>
+            <Pressable>
+              <Block
+                backgroundColor={colors.goldenrod}
+                paddingHorizontal={20}
+                paddingVertical={10}
+                borderRadius={10}
+                elevation={1}
+              >
+                <Text bold color={colors.gray}>
+                  Pay now
+                </Text>
+              </Block>
+            </Pressable>
+          </Block>
+        </Block>
       </BottomSheet>
-    </View>
+    </Block>
   );
 };
 
@@ -35,7 +79,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    backgroundColor: 'grey',
+    backgroundColor: 'black',
   },
   contentContainer: {
     flex: 1,
